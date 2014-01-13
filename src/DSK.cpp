c264539009be48c88607650fa796c103e893d554
edrezen <edrezen@irisa.fr>
2014-01-13 16:14:49 +0100

    Update due to API changes in gatb-core

--- src/DSK.cpp
+++ src/DSK.cpp
@@ -13,14 +13,6 @@
 
 using namespace std;
 
-/********************************************************************************/
-
-#if 1
-#define ProductFactoryCurrent ProductFileFactory
-#else
-#define ProductFactoryCurrent ProductHDF5Factory
-#endif
-
 /*********************************************************************
 ** METHOD  :
 ** PURPOSE :
@@ -29,15 +21,16 @@ using namespace std;
 ** RETURN  :
 ** REMARKS :
 *********************************************************************/
-template<typename T>
+template<size_t span>
 static void executeAlgorithm (DSK& dsk, IProperties* props)
 {
-    IBank* bank = new Bank (props->getStr(STR_URI_DB));
+    IBank* bank = new BankFasta (props->getStr(STR_URI_DB));
     LOCAL (bank);
 
-    size_t kmerSize = props->get(STR_KMER_SIZE)  ? props->getInt(STR_KMER_SIZE) : 27;
+    size_t kmerSize = props->get(STR_KMER_SIZE)  ? props->getInt(STR_KMER_SIZE) : 31;
     size_t nks      = props->get(STR_NKS)        ? props->getInt(STR_NKS)       : 3;
-    string bargraph = props->get(STR_VERBOSE) ?  "2" : "0";
+
+    StorageMode_e storageMode =  props->getInt(STR_OUTPUT_FORMAT) == 0 ?  STORAGE_FILE : STORAGE_HDF5;
 
     string output = props->get(STR_URI_OUTPUT) ?
         props->getStr(STR_URI_OUTPUT)   :
@@ -46,9 +39,9 @@ static void executeAlgorithm (DSK& dsk, IProperties* props)
     string binaryBankUri = System::file().getCurrentDirectory() + "/bank.bin";
 
     /************************************************************/
-    /*                       Product creation                   */
+    /*                       Storage creation                   */
     /************************************************************/
-    Product<ProductFactoryCurrent>* product = ProductFactoryCurrent::createProduct (output, true, false);
+    Storage* product = StorageFactory(storageMode).createStorage (output, true, false);
     LOCAL (product);
 
     /************************************************************/
@@ -56,7 +49,7 @@ static void executeAlgorithm (DSK& dsk, IProperties* props)
     /************************************************************/
     /** We create the binary bank. */
     BankConverterAlgorithm converter (bank, kmerSize, binaryBankUri);
-    converter.getInput()->add (0, STR_PROGRESS_BAR, bargraph);
+    converter.getInput()->add (0, STR_VERBOSE, props->getStr(STR_VERBOSE));
     converter.execute();
     dsk.getInfo()->add (1, converter.getInfo());
 
@@ -64,7 +57,7 @@ static void executeAlgorithm (DSK& dsk, IProperties* props)
     /*                         Sorting count                    */
     /************************************************************/
     /** We create a DSK instance and execute it. */
-    SortingCountAlgorithm<ProductFactoryCurrent, T> sortingCount (
+    SortingCountAlgorithm<span> sortingCount (
         product,
         converter.getResult(),
         kmerSize,
@@ -73,7 +66,7 @@ static void executeAlgorithm (DSK& dsk, IProperties* props)
         props->get(STR_MAX_DISK)   ? props->getInt(STR_MAX_DISK)   : 0,
         props->get(STR_NB_CORES)   ? props->getInt(STR_NB_CORES)   : 0
     );
-    sortingCount.getInput()->add (0, STR_PROGRESS_BAR, bargraph);
+    sortingCount.getInput()->add (0, STR_VERBOSE, props->getStr(STR_VERBOSE));
     sortingCount.execute();
     dsk.getInfo()->add (1, sortingCount.getInfo());
 
@@ -94,16 +87,18 @@ static void executeAlgorithm (DSK& dsk, IProperties* props)
 *********************************************************************/
 DSK::DSK () : Tool ("dsk")
 {
-    /** We add options specific to DSK. */
-    getParser()->add (new OptionOneParam (STR_URI_DB,          "databank uri",                         true));
-    getParser()->add (new OptionOneParam (STR_KMER_SIZE,       "size of a kmer",                       true            ));
-    getParser()->add (new OptionOneParam (STR_MAX_MEMORY,      "max memory in MBytes",                 false,  "1000"  ));
-    getParser()->add (new OptionOneParam (STR_MAX_DISK,        "max disk space in MBytes",             false,  "0"     ));
-    getParser()->add (new OptionOneParam (STR_NKS,             "abundance threshold for solid kmers",  false,  "3"     ));
-    getParser()->add (new OptionOneParam (STR_KMER_SOLID,      "solid kmers file",                     false,  "solid" ));
-    getParser()->add (new OptionOneParam (STR_URI_HISTOGRAM,   "outputs histogram of kmers abundance", false));
-    getParser()->add (new OptionOneParam (STR_PARTITION_TYPE,  "partitioning type : 0 for map (default), 1 for vector", false, "0"));
-    getParser()->add (new OptionOneParam (STR_PREFIX,          "prefix for output files",              false, "tmp."));
+    /** We add options specific to DSK (most important at the end). */
+    getParser()->push_front (new OptionOneParam (STR_VERBOSE,         "verbosity level",                      false,  "1"));
+    getParser()->push_front (new OptionOneParam (STR_OUTPUT_FORMAT,   "output format (0 binary, 1 HDF5)",     false,  "0"));
+    getParser()->push_front (new OptionOneParam (STR_PREFIX,          "prefix for output files",              false, "tmp."));
+    getParser()->push_front (new OptionOneParam (STR_PARTITION_TYPE,  "partitioning type : 0 for map (default), 1 for vector", false, "0"));
+    getParser()->push_front (new OptionOneParam (STR_URI_HISTOGRAM,   "outputs histogram of kmers abundance", false));
+    getParser()->push_front (new OptionOneParam (STR_KMER_SOLID,      "solid kmers file",                     false,  "solid" ));
+    getParser()->push_front (new OptionOneParam (STR_NKS,             "abundance threshold for solid kmers",  false,  "3"     ));
+    getParser()->push_front (new OptionOneParam (STR_MAX_DISK,        "max disk space in MBytes",             false,  "0"     ));
+    getParser()->push_front (new OptionOneParam (STR_MAX_MEMORY,      "max memory in MBytes",                 false,  "1000"  ));
+    getParser()->push_front (new OptionOneParam (STR_KMER_SIZE,       "size of a kmer",                       true            ));
+    getParser()->push_front (new OptionOneParam (STR_URI_DB,          "databank uri",                         true));
 }
 
 /*********************************************************************
@@ -120,9 +115,9 @@ void DSK::execute ()
     size_t kmerSize = getInput()->getInt (STR_KMER_SIZE);
 
     /** According to the kmer size, we instantiate one DSKAlgorithm class and delegate the actual job to it. */
-         if (kmerSize < 32)   { executeAlgorithm <LargeInt<1> > (*this, getInput());  }
-    else if (kmerSize < 64)   { executeAlgorithm <LargeInt<2> > (*this, getInput());  }
-    else if (kmerSize < 96)   { executeAlgorithm <LargeInt<3> > (*this, getInput());  }
-    else if (kmerSize < 128)  { executeAlgorithm <LargeInt<4> > (*this, getInput());  }
+         if (kmerSize < 32)   { executeAlgorithm <32>  (*this, getInput());  }
+    else if (kmerSize < 64)   { executeAlgorithm <64>  (*this, getInput());  }
+    else if (kmerSize < 96)   { executeAlgorithm <96>  (*this, getInput());  }
+    else if (kmerSize < 128)  { executeAlgorithm <128> (*this, getInput());  }
     else  { throw Exception ("unsupported kmer size %d", kmerSize);  }
 }
